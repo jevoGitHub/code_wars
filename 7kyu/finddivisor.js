66781250ba001bb48aab2465b651c72370a932ad
jevogithub <johnjevora94@gmail.com>
2023-04-23 23:18:29 -0400

    updating codewars

---
+++ 7kyu/finddivisor.js
@@ -0,0 +1,20 @@
+/*
+Create a function named divisors/Divisors that takes an integer 
+n > 1 and returns an array with all of the integer's divisors
+(except for 1 and the number itself), from smallest to largest. 
+If the number is prime return the string '(integer) is prime'
+*/
+
+function divisors(integer) {
+    const divisorArr = []
+    for (let i = 1; i < integer; i++) {
+        if (integer % i === 0 && i > 1) {
+            divisorArr.push(i)
+        }
+    }
+    if (divisorArr.length === 0) {
+        return `${integer} is prime`
+    } else {
+        return divisorArr
+    }
+};
