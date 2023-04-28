02a9f913c7a080b1ef37728a4e59c2a63018693d
jevogithub <johnjevora94@gmail.com>
2023-04-27 23:37:19 -0400

    updating codewars

---
+++ 6kyu/stringtocamelcase.js
@@ -0,0 +1,10 @@
+function toCamelCase(str) {
+    const words = str.split(/[-_]/);
+    let result = words[0];
+
+    for (let i = 1; i < words.length; i++) {
+        result += words[i][0].toUpperCase() + words[i].slice(1);
+    }
+
+    return result;
+}
