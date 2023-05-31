4c293ed6f66ac374fc14b207473e7bcaf233e27e
jevogithub <johnjevora94@gmail.com>
2023-05-31 16:00:03 -0400

    updating codewars

---
+++ 6kyu/isnumprime.js
@@ -0,0 +1,15 @@
+//Define a function that takes an integer argument and returns a logical value true or false /
+//depending on if the integer is a prime.
+
+function isPrime(num) {
+    if (num < 2) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
